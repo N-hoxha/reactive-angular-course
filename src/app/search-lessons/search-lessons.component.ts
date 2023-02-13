dc3e2d7fa09f83f17366e9ccfd2abe50633ea8bb
nhoxha95 <neritan.hoxha@innovaway.al>
2023-02-13 15:29:29 +0100

    Build search function in search-lessons and lesson components

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { CoursesService } from '../service/courses.service';
 
 
 @Component({
@@ -24,16 +25,34 @@ import {Lesson} from '../model/lesson';
 })
 export class SearchLessonsComponent implements OnInit {
 
-  constructor() {
+  searchResults$: Observable<Lesson[]>; // data store for array objekt
+
+  activeLesson: Lesson; // data store for only one object 
+
+  constructor(private coursesService: CoursesService) {
 
 
   }
-
+ 
   ngOnInit() {
 
 
   }
 
+  onSearch(search: string) {
+
+    this.searchResults$ = this.coursesService.searchLesson(search);
+
+  }
+
+  openLesson(lesson: Lesson) { // take lesson objekt
+   console.log("take lesson objekt", lesson);
+   this.activeLesson = lesson; // mbushim objektin
+  }
+
+  onBackToSearch() {
+    this.activeLesson = null;  // fshim te dhenat 
+  }
 }
 
 
--- src/app/service/courses.service.ts
+++ src/app/service/courses.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Course } from '../model/course';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Lesson } from '../model/lesson';
 
 @Injectable({
   providedIn: 'root'
@@ -28,5 +29,17 @@ export class CoursesService {
   //     shareReplay()
   //   );
   //  }
+
+  searchLesson(search: string): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        filter: search,
+        pageSize: "100"
+      }
+    }).pipe(
+      map(res => res["payload"]),
+      shareReplay()
+    );
+  }
 
 } 
